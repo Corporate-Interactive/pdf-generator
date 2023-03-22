1ee7a294d7e704459ac1f812195906bcadef1420
nickle-gao <86577075+nickle-gao@users.noreply.github.com>
2023-03-23 06:43:06 +0800

    generating pdf to be called by worker fixing broken links in TOC

--- src/main/java/com/corporateinteractive/pdfgenerator/controllers/PdfGenerationController.java
+++ src/main/java/com/corporateinteractive/pdfgenerator/controllers/PdfGenerationController.java
@@ -1,18 +1,17 @@
 package com.corporateinteractive.pdfgenerator.controllers;
 
-import com.corporateinteractive.pdfgenerator.services.PdfGenerationService;
-import com.itextpdf.styledxmlparser.jsoup.internal.StringUtil;
-import io.micrometer.common.util.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
+import com.corporateinteractive.pdfgenerator.services.PdfGenerationService;
+import com.google.gson.Gson;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class PdfGenerationController {
@@ -24,24 +23,9 @@ public class PdfGenerationController {
     }
 
     @GetMapping(value = "/generate-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> generatePdf(@RequestParam(name="pdfURL", required=false) String pdfURL) throws IOException {
-        if (StringUtils.isEmpty(pdfURL)) {
-            pdfURL = "http://localhost:8000/application/pdf/pdf.jsp?email=emailforpdf&proposal=109068&v-key=7aafbd2e260355a675704bf6b0c286388791142b&userId=4806";
-        }
-        byte[] pdfBytes = pdfGenerationService.generatePdfFromUrl(pdfURL);
-        return ResponseEntity.ok().body(pdfBytes);
-    }
-
-    @GetMapping(value = "/merge-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> mergePdf(@RequestParam(name="pdfURL", required=false) String pdfURL) throws IOException {
-        byte[] pdfBytes = pdfGenerationService.generatePdfFromUrl(pdfURL);
-        return ResponseEntity.ok().body(pdfBytes);
-    }
-
-    @GetMapping(value = "/split-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> splitPdf(@RequestParam(name="pdfURL", required=false) String pdfURL) throws IOException {
-        byte[] pdfBytes = pdfGenerationService.generatePdfFromUrl(pdfURL);
-        return ResponseEntity.ok().body(pdfBytes);
+    public void generatePdf(@RequestParam(name="pdfURL", required=false) String pdfURL, HttpServletResponse response) throws IOException, URISyntaxException {
+//        	pdfUrl = "http://localhost:8080/application/pdf/pdf.jsp?email=emailforpdf&proposal=11564&v-key=2d83fe6c6c65a2220e74b7b488c672ff46d543f3&userId=38";
+        pdfGenerationService.generatePdfFromUrl(pdfURL, response.getOutputStream());
     }
 
 }
--- src/main/java/com/corporateinteractive/pdfgenerator/services/PdfGenerationService.java
+++ src/main/java/com/corporateinteractive/pdfgenerator/services/PdfGenerationService.java
@@ -1,92 +1,94 @@
 package com.corporateinteractive.pdfgenerator.services;
-import com.itextpdf.html2pdf.ConverterProperties;
-import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.kernel.geom.PageSize;
-import com.itextpdf.layout.font.FontProvider;
-import com.itextpdf.styledxmlparser.css.media.MediaDeviceDescription;
-import com.itextpdf.styledxmlparser.css.media.MediaType;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.styledxmlparser.css.media.MediaDeviceDescription;
+import com.itextpdf.styledxmlparser.css.media.MediaType;
+
 @Service
 public class PdfGenerationService {
-    private static final Logger LOGGER = LogManager.getLogger(PdfGenerationService.class);
+	private static final Logger LOGGER = LogManager.getLogger(PdfGenerationService.class);
 
-    public byte[] generatePdfFromUrl(String pdfUrl) throws IOException {
+	private String getBaseUri(String pdfUrl) throws URISyntaxException {
+		URI uri = new URI(pdfUrl);
+		return uri.getScheme() + "://" + uri.getHost() + (uri.getPort() > 0 ? ":" + uri.getPort() : "");
+	}
 
-        String requestURL = pdfUrl;
-        try {
-            URI uri = new URI(pdfUrl);
-            requestURL = uri.getScheme() + "://" +uri.getHost() + (StringUtils.isNotBlank(uri.getPort() + "") ? ":" + uri.getPort() : "");
-        } catch (URISyntaxException e) {
-            System.out.println("Invalid URL: " + e.getMessage());
-            return null;
-        }
+	public void generatePdfFromUrl(String pdfUrl, OutputStream os) throws IOException, URISyntaxException {
 
-        LOGGER.info("Setting PDF Document: " + pdfUrl);
-        URL url = new URL(pdfUrl);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ConverterProperties properties = new ConverterProperties();
-        properties.setBaseUri(requestURL);
-        FontProvider fontProvider = new FontProvider();
-        fontProvider.addDirectory("/Users/coreybaines/CITAUS/stores/QCLOUD/_fonts/unicode");
-        fontProvider.addDirectory("/Users/coreybaines/CITAUS/stores/QCLOUD/_application_fonts");
-        properties.setFontProvider(fontProvider);
-        MediaDeviceDescription mediaDeviceDescription =
-                new MediaDeviceDescription(MediaType.SCREEN);
-        mediaDeviceDescription.setWidth(PageSize.A4.getWidth());
-        properties.setMediaDeviceDescription(mediaDeviceDescription);
-        HtmlConverter.convertToPdf(url.openStream(), outputStream, properties);
-        return outputStream.toByteArray();
-    }
+		String storeDir = "/stores/QCLOUD";
+		LOGGER.info("Setting PDF Document: " + pdfUrl);
+		URL url = new URL(pdfUrl);
+		ConverterProperties properties = new ConverterProperties();
+		properties.setBaseUri(getBaseUri(pdfUrl));
+		FontProvider fontProvider = new FontProvider();
 
-    private static List<File> traverseFonts(File... folders)
-    {
-        List<File> fonts = new ArrayList<>();
-        FileFilter foldersAndAcceptedFontFiles = new FileFilter()
-        {
-            @Override
-            public boolean accept(File pathname)
-            {
-                if (pathname.isDirectory())
-                {
-                    return true;
-                }
-                return pathname.getName().toLowerCase().endsWith(".otf") || pathname.getName().toLowerCase().endsWith(".ttf");
-            }
-        };
-        for (File folder : folders)
-        {
-            File[] listFiles = folder.listFiles(foldersAndAcceptedFontFiles);
-            if (listFiles == null)
-            {
-                continue;
-            }
-            for (File file : listFiles)
-            {
-                if (file.isDirectory())
-                {
-                    fonts.addAll(traverseFonts(file));
-                }
-                else
-                {
-                    fonts.add(file);
-                }
-            }
-        }
+		List<File> fontFiles = traverseFonts(new File(storeDir, "_fonts/unicode"),
+				new File(storeDir, "_application_fonts"));
+		for (File fontFile : fontFiles) {
+			fontProvider.addFont(fontFile.getAbsolutePath());
+		}
+		properties.setFontProvider(fontProvider);
+		MediaDeviceDescription mediaDeviceDescription = new MediaDeviceDescription(MediaType.SCREEN);
+		// mediaDeviceDescription.setWidth(PageSize.A4.getWidth());
+		properties.setMediaDeviceDescription(mediaDeviceDescription);
+		HtmlConverter.convertToPdf(url.openStream(), os, properties);
 
-        return fonts;
-    }
+	}
+
+	private static List<File> traverseFonts(File... folders) {
+		List<File> fonts = new ArrayList<>();
+		FileFilter foldersAndAcceptedFontFiles = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				if (pathname.isDirectory()) {
+					return true;
+				}
+				return pathname.getName().toLowerCase().endsWith(".otf")
+						|| pathname.getName().toLowerCase().endsWith(".ttf");
+			}
+		};
+		for (File folder : folders) {
+			File[] listFiles = folder.listFiles(foldersAndAcceptedFontFiles);
+			if (listFiles == null) {
+				continue;
+			}
+			for (File file : listFiles) {
+				if (file.isDirectory()) {
+					fonts.addAll(traverseFonts(file));
+				} else {
+					fonts.add(file);
+				}
+			}
+		}
+
+		return fonts;
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		String url = "http://localhost:8080/application/pdf/pdf.jsp?email=emailforpdf&proposal=11564&v-key=2d83fe6c6c65a2220e74b7b488c672ff46d543f3&userId=38";
+		OutputStream os = new FileOutputStream(new File("/temp/pdf-110319-7-" + RandomStringUtils.randomAlphabetic(3) + ".pdf"));
+		new PdfGenerationService().generatePdfFromUrl(url,
+				os );
+		os.close();
+		LOGGER.info("done");
+	}
+
 }
