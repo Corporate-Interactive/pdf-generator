7d94f4aa1ebe00620f533fd8cce16f41e14098a7
VuCIT <vu.tran@corporateinteractive.com.au>
2023-07-20 09:07:55 +1000

    Updated pdf generator to use post mapping

--- src/main/java/com/corporateinteractive/pdfgenerator/controllers/PdfGenerationController.java
+++ src/main/java/com/corporateinteractive/pdfgenerator/controllers/PdfGenerationController.java
@@ -2,14 +2,16 @@ package com.corporateinteractive.pdfgenerator.controllers;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corporateinteractive.pdfgenerator.services.PdfGenerationService;
-import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -22,9 +24,11 @@ public class PdfGenerationController {
         this.pdfGenerationService = pdfGenerationService;
     }
 
-    @GetMapping(value = "/generate-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public void generatePdf(@RequestParam(name="pdfURL", required=false) String pdfURL, HttpServletResponse response) throws IOException, URISyntaxException {
+    @PostMapping(value = "/generate-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+//    public void generatePdf(@RequestParam(name="pdfURL", required=false) String pdfURL, HttpServletResponse response) throws IOException, URISyntaxException {
+    public void generatePdf(@RequestBody Map<String,Object> body, HttpServletResponse response) throws IOException, URISyntaxException {
 //        	pdfUrl = "http://localhost:8080/application/pdf/pdf.jsp?email=emailforpdf&proposal=11564&v-key=2d83fe6c6c65a2220e74b7b488c672ff46d543f3&userId=38";
+        String pdfURL = (String) body.get("pdfURL");
         pdfGenerationService.generatePdfFromUrl(pdfURL, response.getOutputStream());
     }
 
